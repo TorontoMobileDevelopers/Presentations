1e5da2fb2d2245fc32a3a05c081e0017d5df8497
danielcauser@gmail.com <Daniel Causer>
2018-05-12 22:00:58 -0400

    [DC] New sample app

---
+++ 2018-05-15_Build_HttpTracer_XF3/HttpTracerSample/HttpTracer.TestApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpTracer.TestApp.ViewModels
+{
+    public class ViewModelBase : BindableBase, INavigationAware, IDestructible
+    {
+        protected INavigationService NavigationService { get; private set; }
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ViewModelBase(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationParameters parameters)
+        {
+            
+        }
+
+        public virtual void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void Destroy()
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+    }
+}
