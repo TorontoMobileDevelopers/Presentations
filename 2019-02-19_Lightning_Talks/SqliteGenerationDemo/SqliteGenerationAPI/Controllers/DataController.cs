d6fd9d1d237f8fae429927e52dc7ad1e859ca689
Daniel Causer <danielcauser@gmail.com>
2019-02-14 19:34:47 -0500

    [DC] Adding Todo Seed

--- 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Controllers/DataController.cs
+++ 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Controllers/DataController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using SqliteGenerationAPI.Datas;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,9 +23,16 @@ namespace SqliteGenerationAPI.Controllers
             _dbContext = dbContext;
             if (_dbContext.TodoItems.Count() == 0)
             {
-                // Create a new TodoItem if collection is empty,
-                // which means you can't delete all TodoItems.
-                _dbContext.TodoItems.Add(new TodoItem { Name = "Item1" });
+                // Seed
+                var list = new List<TodoItem>();
+
+                using (var reader = new StreamReader(Assembly.GetAssembly(typeof(Startup))
+                                                               .GetManifestResourceStream(Assembly.GetAssembly(typeof(Startup)).GetManifestResourceNames().First()) ?? throw new InvalidOperationException()))
+                {
+                    list = JsonConvert.DeserializeObject<List<TodoItem>>(reader.ReadToEnd());
+                }
+
+                _dbContext.TodoItems.AddRange(list);
                 _dbContext.SaveChanges();
             }
         }
