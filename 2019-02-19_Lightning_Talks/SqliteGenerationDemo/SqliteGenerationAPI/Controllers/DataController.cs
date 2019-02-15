e4268eadfbe6cf6c44ddd8cbee2d273f7eb44f8f
Daniel Causer <danielcauser@gmail.com>
2019-02-14 19:12:05 -0500

    [DC] Addinc basic db context

--- 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Controllers/DataController.cs
+++ 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Controllers/DataController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SqliteGenerationAPI.Datas;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +13,25 @@ namespace SqliteGenerationAPI.Controllers
     [Route("api/[controller]")]
     public class DataController : Controller
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly DataContext _dbContext;
+
+        public DataController(DataContext dbContext)
         {
-            return new string[] { "value1", "value2" };
+            _dbContext = dbContext;
+            if (_dbContext.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _dbContext.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _dbContext.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _dbContext.TodoItems.ToListAsync();
         }
 
         [HttpGet("GenerateSqlite")]
--- 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Startup.cs
+++ 2019-02-19_Lightning_Talks/SqliteGenerationDemo/SqliteGenerationAPI/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SqliteGenerationAPI.Datas;
 using Swashbuckle.AspNetCore.Swagger;
 
 
@@ -27,6 +29,9 @@ namespace SqliteGenerationAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSwaggerGen(c =>
